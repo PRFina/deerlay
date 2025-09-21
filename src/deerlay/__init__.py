06b972c2b43560c71fe81405935cad1d007ec2a0
PRFina <PRFina@users.noreply.github.com>
2025-09-21 21:26:28 +0200

    refactor!: enhance API naming for improved clarity and consistency
    This commit introduces better type annotations and consolidates
    shared logic into a base class, improving maintainability and readability.
    
    BREAKING CHANGE: Renamed API methods, arguments, and class names to align with
    a clearer and more consistent API design.

--- src/deerlay/__init__.py
+++ src/deerlay/__init__.py
@@ -1,8 +1,8 @@
-from collections.abc import Callable, Generator, Iterable
+from abc import ABC, abstractmethod
+from collections.abc import Generator, Iterable
 import os
 from pathlib import Path
 import re
-from typing import Any
 
 import pandas as pd
 
@@ -10,7 +10,7 @@ FileEntry = tuple[Path, dict[str, str]]
 FileEntries = Iterable[FileEntry]
 
 
-NOT_ALLOWED_SEPARATORS = {
+NOT_ALLOWED_DELIMITERS = {
     "/",
     "\\",
     ":",
@@ -23,29 +23,36 @@ NOT_ALLOWED_SEPARATORS = {
 }
 
 
-# TODO declare as ABC
-class DirectoryLayout:
-    def __init__(self, root_dir: Path, keyval_separator="="):
-        if keyval_separator in NOT_ALLOWED_SEPARATORS:
-            msg = f"'{keyval_separator}' character is not allowed as separator. \
-                Select another one not included in {NOT_ALLOWED_SEPARATORS}"
-            raise ValueError(msg)
+def check_delimiter(field_name_delimiter: str) -> bool:
+    if field_name_delimiter in NOT_ALLOWED_DELIMITERS:
+        msg = f"'{field_name_delimiter}' character is not allowed as field_name_delimiter. \
+                Select another one not included in {NOT_ALLOWED_DELIMITERS}"
+        raise ValueError(msg)
 
+
+class DirectoryLayout(ABC):
+    def __init__(self, root_dir: Path):
         self.root_dir = Path(root_dir)
         if not self.root_dir.exists():
             raise FileNotFoundError(f"Root directory '{self.root_dir}' does not exist.")
 
-        self.keyval_separator = keyval_separator
+    def collect(self) -> Generator[FileEntry, None, None]:
+        for filepath in self.discover():
+            filepath, metadata = self.parse(filepath)
 
-    def discover(self) -> Generator[FileEntries, None, None]:
-        raise NotImplementedError
+            yield filepath, metadata
+
+    @abstractmethod
+    def discover(self, filepath: Path) -> Generator[Path, None, None]:
+        pass
+
+    @abstractmethod
+    def parse(self, filepath: Path) -> FileEntry:
+        pass
 
     def get_fullpath(self, filename, as_absolute=False) -> Path:
         filepath = self.root_dir / filename
         return filepath.absolute() if as_absolute else filepath
-
-    def _parse_entry(self, filepath: Path) -> FileEntry:
-        raise NotImplementedError
 
     def build_index_table(
         self,
@@ -66,24 +73,49 @@ class DirectoryLayout:
 
 
 class FlatLayout(DirectoryLayout):
-    def __init__(self, root_dir: Path, fields: list[str], field_separator: str = "$"):
-        super().__init__(root_dir, None)
+    def __init__(self, root_dir: Path, fields: list[str], field_delimiter: str = "$"):
+        super().__init__(root_dir)
+        check_delimiter(field_delimiter)
+
         self.fields = fields
-        self.field_separator = field_separator
+        self.field_delimiter = field_delimiter
         self._discover_pattern = "*.*"
-        self._pattern = re.compile(rf"([^{field_separator}]+)")
+        self._pattern = re.compile(rf"([^{field_delimiter}]+)")
 
-    def discover(self):
-        for path in Path(self.root_dir).glob(self._discover_pattern):
-            yield self._parse_entry(path)
+    def discover(self) -> Generator[Path, None, None]:
+        yield from self.root_dir.glob(self._discover_pattern)
 
-    def _parse_entry(self, filepath: Path):
+    def parse(self, filepath: Path) -> FileEntry:
         filepath = filepath.relative_to(self.root_dir)
 
         matches = re.findall(self._pattern, str(filepath))
-        parsed_components = dict(zip(self.fields, matches, strict=False))
+        meta = dict(zip(self.fields, matches, strict=False))
 
-        return filepath, parsed_components
+        return filepath, meta
+
+
+class NamedFlatLayout(DirectoryLayout):
+    def __init__(self, root_dir: Path, field_name_delimiter: str = "=", field_delimiter: str = "$"):
+        super().__init__(root_dir)
+        check_delimiter(field_name_delimiter)
+        check_delimiter(field_delimiter)
+
+        self.field_delimiter = field_delimiter
+        self._discover_pattern = "*.*"
+        self._pattern = re.compile(
+            rf"([^${field_name_delimiter}]+){field_name_delimiter}([^${field_delimiter}\.]+)"
+        )
+
+    def discover(self) -> Generator[Path, None, None]:
+        yield from self.root_dir.glob(self._discover_pattern)
+
+    def parse(self, filepath: Path) -> FileEntry:
+        filepath = filepath.relative_to(self.root_dir)
+
+        matches = re.findall(self._pattern, filepath.name)
+        meta = dict(matches)
+
+        return filepath, meta
 
 
 class HierarchicalLayout(DirectoryLayout):
@@ -93,71 +125,47 @@ class HierarchicalLayout(DirectoryLayout):
         DirectoryLayout (_type_): _description_
     """
 
-    def __init__(
-        self, root_dir: Path, fields: list[str], segment_separator: str = os.path.sep
-    ) -> None:
-        super().__init__(root_dir, None)
-        self.segment_separator = segment_separator
+    def __init__(self, root_dir: Path, fields: list[str]) -> None:
+        super().__init__(root_dir)
+        self.field_delimiter = os.path.sep
         self.fields = fields
-        self._pattern = re.compile(rf"([^{segment_separator}]+)")
+        self._pattern = re.compile(rf"([^{self.field_delimiter}]+)")
 
-    def discover(self, filter_func: Callable[[FileEntry], bool] | None = None) -> Any:
+    def discover(self) -> Generator[Path, None, None]:
         for dirpath, _, filenames in os.walk(self.root_dir):
             if filenames:
                 for filename in filenames:
                     filepath = Path(dirpath) / filename
-                    yield self._parse_entry(filepath)
+                    yield filepath
 
-    def _parse_entry(self, filepath: Path) -> tuple[Path, dict[str, str]]:
+    def parse(self, filepath: Path) -> FileEntry:
         filepath = filepath.relative_to(self.root_dir)
+        dirpath, filename = filepath.parent, filepath.name
 
-        matches = re.findall(self._pattern, str(filepath))
-        parsed_components = dict(zip(self.fields, matches, strict=False))
+        meta = dict(zip(self.fields, dirpath.parts, strict=False))
+        meta["filename"] = filename
 
-        return filepath, parsed_components
-
-
-class FlatLayoutWithKey(DirectoryLayout):
-    def __init__(self, root_dir: Path, keyval_separator: str = "=", field_separator: str = "$"):
-        super().__init__(root_dir, keyval_separator)
-        self.field_separator = field_separator
-        self._discover_pattern = "*.*"
-        self._pattern = re.compile(
-            rf"([^${keyval_separator}]+){keyval_separator}([^${field_separator}\.]+)"
-        )
-
-    def discover(self):
-        for path in Path(self.root_dir).glob(self._discover_pattern):
-            yield self._parse_entry(path)
-
-    def _parse_entry(self, filepath: Path):
-        filepath = filepath.relative_to(self.root_dir)
-        matches = re.findall(self._pattern, filepath.name)
-
-        # Convert matches into a dictionary
-        parsed_components = dict(matches)
-        return filepath, parsed_components
+        return filepath, meta
 
 
-class HierarchicalLayoutWithKey(DirectoryLayout):
-    def __init__(self, root_dir: Path, keyval_separator: str = "=", segment_separator: str = "$"):
-        super().__init__(root_dir, keyval_separator)
-        self.segment_separator = segment_separator
-        self._pattern = re.compile(
-            rf"([^${keyval_separator}]+){keyval_separator}([^${segment_separator}\.]+)"
-        )
+class NamedHierarchicalLayout(DirectoryLayout):
+    def __init__(self, root_dir: Path, field_name_delimiter: str = "="):
+        super().__init__(root_dir)
+        check_delimiter(field_name_delimiter)
+        self.keyval_separator = field_name_delimiter
 
-    def discover(self) -> Any:
+    def discover(self) -> Generator[Path, None, None]:
         for dirpath, _, filenames in os.walk(self.root_dir):
             if filenames:
                 for filename in filenames:
                     filepath = Path(dirpath) / filename
-                    yield self._parse_entry(filepath)
+                    yield filepath
 
-    def _parse_entry(self, filepath: Path) -> tuple[Path, dict[str, str]]:
+    def parse(self, filepath: Path) -> FileEntry:
         filepath = filepath.relative_to(self.root_dir)
-        for part in filepath.parts:
-            matches = re.findall(self._pattern, part)
-            parsed_components = dict(matches)
+        dirpath, filename = filepath.parent, filepath.name
 
-        return filepath, parsed_components
+        meta = dict(part.split(self.keyval_separator, 1) for part in dirpath.parts)
+        meta["filename"] = filename
+
+        return filepath, meta
